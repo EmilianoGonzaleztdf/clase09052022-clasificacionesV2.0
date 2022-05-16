117145fc9ab87aa23241ab7812d5833fd53966b7
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-15 22:24:52 -0300

    v2.1 codigo con chiches

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 let prom, nt1, nt2, nt3, sumaNota: number;
-let nombre: string = " ";
+
+let nombre = prompt("ingrese el nombre de Alumno:");
+console.log("Alumno :" + nombre);
 
 while (nombre !== "") {
-  nombre = prompt("ingrese el nombre de Alumno:");
-  console.log("Alumno :" + nombre);
   nt1 = Number(prompt("ingrese calificacion de la Practica"));
-  console.log("Calificacion Practica :" + nt1);
   nt2 = Number(prompt("ingrese calificacion del Problema"));
-  console.log("Calificacion Problema :" + nt2);
   nt3 = Number(prompt("ingrese calificacion del Teorico"));
-  console.log("Calificacion Teorico :" + nt3);
+  console.log(
+    "Calificacion Practica :" + nt1,
+    "Calificacion Problema :" + nt2,
+    "Calificacion Teorico :" + nt3
+  );
 
   if (nt1 > -1 && nt2 > -1 && nt3 > -1 && nt1 <= 10 && nt2 <= 10 && nt3 <= 10) {
     prom = console.log(
@@ -21,4 +23,8 @@ while (nombre !== "") {
   } else {
     console.log("error, usted ingreso un valor errones");
   }
-} //v2.0
+  nombre = prompt("ingrese el nombre de Alumno:");
+  if (nombre !== "") {
+    console.log("Alumno :" + nombre);
+  }
+}

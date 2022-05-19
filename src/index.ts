6c915b2ab078d2296e9954b95c6882b15c9514ad
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-19 20:11:07 -0300

    corregido en clase

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-let prom, nt1, nt2, nt3, sumaNota: number;
+let prom, nt1, nt2, nt3: number;
 
 let nombre = prompt("ingrese el nombre de Alumno:");
 console.log("Alumno :" + nombre);
@@ -14,14 +14,14 @@ while (nombre !== "") {
   );
 
   if (nt1 > -1 && nt2 > -1 && nt3 > -1 && nt1 <= 10 && nt2 <= 10 && nt3 <= 10) {
-    prom = console.log(
+    console.log(
       "el promedio de",
       nombre,
       "es",
       nt1 * 0.1 + nt2 * 0.5 + nt3 * 0.4
     );
   } else {
-    console.log("error, usted ingreso un valor errones");
+    console.log("error, usted ingreso un valor con errones");
   }
   nombre = prompt("ingrese el nombre de Alumno:");
   if (nombre !== "") {
